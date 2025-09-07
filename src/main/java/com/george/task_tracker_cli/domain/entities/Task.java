bdbcfa564dca2d447ceff4b637c1e1674370996c
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-07 20:15:14 +0400

    for setting priority for tasks. organizing relationship between tasklist and task classes

---
+++ src/main/java/com/george/task_tracker_cli/domain/entities/Task.java
@@ -0,0 +1,167 @@
+package com.george.task_tracker_cli.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@SuppressWarnings("JpaAttributeTypeInspection")
+@Entity
+@Table(name="tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name="title",nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name="due_Date")
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status",nullable = false)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="priority",nullable = false)
+    private TaskPriority priority;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_list_id", nullable = false)
+    private TaskList taskList;
+
+
+
+
+    @Column(name="created",nullable = false)
+    private LocalDateTime created;
+
+    @Column(name="updated",nullable = false)
+    private LocalDateTime updated;
+
+    public Task() {
+    }
+
+    public Task(UUID id, String title, String description, LocalDate dueDate, TaskStatus status, TaskPriority priority, LocalDateTime created, LocalDateTime updated, TaskList taskList) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+        this.taskList = taskList;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public TaskPriority getTaskPriority() {
+        return priority;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public void setTaskPriority(TaskPriority taskPriority) {
+        this.priority = taskPriority;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(title, task.title) && Objects.equals(description, task.description) && Objects.equals(dueDate, task.dueDate) && status == task.status && priority == task.priority && Objects.equals(created, task.created) && Objects.equals(updated, task.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, dueDate, status,priority, created, updated);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", Description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", status=" + status +
+                ", taskPriority=" + priority +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+}
