75642da3f2b2cac56e9dd3903824608529356d12
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-09 17:04:54 +0400

    implemented TaskListService.java and TaskListServiceImpl.java

---
+++ src/main/java/com/george/task_tracker_cli/Services/Impl/TaskListServiceImpl.java
@@ -0,0 +1,26 @@
+package com.george.task_tracker_cli.Services.Impl;
+
+import com.george.task_tracker_cli.Repositories.TaskListRepository;
+import com.george.task_tracker_cli.Services.TaskListService;
+import com.george.task_tracker_cli.domain.entities.TaskList;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskListServiceImpl implements TaskListService {
+
+    private final TaskListRepository taskListRepository;
+
+    public TaskListServiceImpl(TaskListRepository taskListRepository) {
+        this.taskListRepository = taskListRepository;
+    }
+
+
+    @Override
+    public List<TaskList> listTaskLists() {
+        return taskListRepository.findAll();
+    }
+
+
+}
---
+++ src/main/java/com/george/task_tracker_cli/Services/TaskListService.java
@@ -0,0 +1,10 @@
+package com.george.task_tracker_cli.Services;
+
+import com.george.task_tracker_cli.domain.entities.TaskList;
+
+import java.util.List;
+
+public interface TaskListService {
+    List<TaskList> listTaskLists();
+
+}
