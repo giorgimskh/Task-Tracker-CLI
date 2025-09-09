cf91bda04466426e862b543ea39f92b79c3745b1
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-09 22:21:11 +0400

    project's first controller implemented

---
+++ src/main/java/com/george/task_tracker_cli/Controllers/TaskListController.java
@@ -0,0 +1,33 @@
+package com.george.task_tracker_cli.Controllers;
+
+import com.george.task_tracker_cli.Repositories.TaskListRepository;
+import com.george.task_tracker_cli.Services.TaskListService;
+import com.george.task_tracker_cli.domain.TaskDto.TaskListDto;
+import com.george.task_tracker_cli.domain.entities.Task;
+import com.george.task_tracker_cli.domain.entities.TaskList;
+import com.george.task_tracker_cli.mappers.TaskListMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/task-lists")
+public class TaskListController {
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMappers;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMappers1) {
+        this.taskListService = taskListService;
+        this.taskListMappers = taskListMappers1;
+
+    }
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMappers::toDto).toList();
+    }
+}
