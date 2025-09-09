1b04ec14f877f7eadf69a4ff0c25fd1607344ce6
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-09 22:32:25 +0400

    implemented method for creating Task List

--- src/main/java/com/george/task_tracker_cli/Services/Impl/TaskListServiceImpl.java
+++ src/main/java/com/george/task_tracker_cli/Services/Impl/TaskListServiceImpl.java
@@ -5,6 +5,7 @@ import com.george.task_tracker_cli.Services.TaskListService;
 import com.george.task_tracker_cli.domain.entities.TaskList;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -24,7 +25,24 @@ public class TaskListServiceImpl implements TaskListService {
 
     @Override
     public TaskList createTaskList(TaskList taskList) {
-        return null;
+        if(null!=taskList.getId()){
+            throw new IllegalArgumentException("Task list already has an ID!");
+        }
+        if(null==taskList.getTitle() || taskList.getTitle().isBlank()){
+            throw new IllegalArgumentException("Task list title is required!");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        return taskListRepository.save(new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+        ));
+
     }
 
 
