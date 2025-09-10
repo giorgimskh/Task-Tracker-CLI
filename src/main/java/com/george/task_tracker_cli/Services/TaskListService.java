fd3915fe5128a32496c615e3ce3b561f225ca918
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-10 23:12:57 +0400

    implemented getTaskList method in interface

--- src/main/java/com/george/task_tracker_cli/Services/TaskListService.java
+++ src/main/java/com/george/task_tracker_cli/Services/TaskListService.java
@@ -1,10 +1,14 @@
 package com.george.task_tracker_cli.Services;
 
+import com.george.task_tracker_cli.domain.entities.Task;
 import com.george.task_tracker_cli.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID taskListId);
 }
