09324529c2c144266fc737388628143f3cece560
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-10 13:27:35 +0400

    repository was not working because of improper naming and parameters.

--- src/main/java/com/george/task_tracker_cli/Repositories/TaskRepository.java
+++ src/main/java/com/george/task_tracker_cli/Repositories/TaskRepository.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
     List<Task> findByTaskList(TaskList taskList);
-    Optional<Task> findByTaskListAndId(UUID taskListId, UUID id);
+    Optional<Task>  findByTaskList_IdAndId(UUID taskListId, UUID id);
 }
