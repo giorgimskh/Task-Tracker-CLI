b6b41217c9a6a701987b186d6c00331df2d9d3cd
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-11 00:36:05 +0400

    create interface for implementing task methods

---
+++ src/main/java/com/george/task_tracker_cli/Services/TaskService.java
@@ -0,0 +1,11 @@
+package com.george.task_tracker_cli.Services;
+
+import com.george.task_tracker_cli.domain.entities.Task;
+import com.george.task_tracker_cli.domain.entities.TaskList;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskService {
+    List<Task> ListTasks(UUID taskListId);
+}
