fbda6d381d8c375d36daaad6f956bbb3648e713e
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-14 17:13:31 +0400

    implementing getTask method in service interface

--- src/main/java/com/george/task_tracker_cli/Services/TaskService.java
+++ src/main/java/com/george/task_tracker_cli/Services/TaskService.java
@@ -4,9 +4,11 @@ import com.george.task_tracker_cli.domain.entities.Task;
 import com.george.task_tracker_cli.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskService {
     List<Task> ListTasks(UUID taskListId);
     Task createTask(UUID taskListId,Task task);
+    Optional <Task> getTask(UUID taskListId, UUID taskId);
 }
